59d70fb4f61ffe365419e813e759400733171668
tar_xzf <tar_xzf@tuta.io>
2023-11-25 11:05:29 +0000

    feat(forward_topic_pins): copy message and add buttons instead of forward

--- src/modules/forward_topic_pins.rs
+++ src/modules/forward_topic_pins.rs
@@ -9,7 +9,9 @@ use std::sync::Arc;
 
 use anyhow::Result;
 use diesel::prelude::*;
+use reqwest::Url;
 use teloxide::prelude::*;
+use teloxide::types::{InlineKeyboardButton, ReplyMarkup};
 use teloxide::utils::html;
 
 use crate::common::{BotEnv, TopicEmojis};
@@ -30,51 +32,57 @@ pub async fn inspect_message<'a>(
     });
     let Some(forward_to) = forward_to else { return Ok(()) };
 
-    let topic_link = render_message_link(&bot, &env, msg).await?;
+    let (link_url, topic_name) = make_message_link(&bot, &env, msg).await?;
 
     if let Some(poll) = msg.poll().filter(|p| !p.is_anonymous) {
         // Polls with visible voters can't be forwarded to channels.
-        bot.send_message(forward_to.to, render_poll(msg, poll, &topic_link))
-            .parse_mode(teloxide::types::ParseMode::Html)
-            .disable_web_page_preview(true)
-            .await?;
-        return Ok(());
-    }
-
-    bot.send_message(forward_to.to, format!("<b>📌 in {topic_link}</b>"))
+        bot.send_message(
+            forward_to.to,
+            render_poll(msg, poll, &html::link(&topic_name, &link_url)),
+        )
         .parse_mode(teloxide::types::ParseMode::Html)
         .disable_web_page_preview(true)
         .await?;
+        return Ok(());
+    }
 
-    bot.forward_message(forward_to.to, msg.chat.id, msg.id).await?;
+    let mut buttons = vec![[InlineKeyboardButton::url(
+        format!("📌 in {topic_name}"),
+        Url::parse(&link_url)?,
+    )]];
+    if let Some(from) = msg.forward_from_user().or(msg.from.as_ref()) {
+        buttons.push([InlineKeyboardButton::url(
+            format!("👤 {}", from.full_name()),
+            Url::parse(&format!("tg://user?id={}", from.id))?,
+        )]);
+    }
+
+    bot.copy_message(forward_to.to, msg.chat.id, msg.id)
+        .reply_markup(ReplyMarkup::inline_kb(buttons))
+        .send()
+        .await?;
 
     Ok(())
 }
 
-async fn render_message_link(
+async fn make_message_link(
     bot: &Bot,
     env: &BotEnv,
     msg: &Message,
-) -> Result<String> {
+) -> Result<(String, String)> {
     let Some(chat_id) = msg.chat.id.channel_t_me_id() else {
-        return Ok(msg
-            .chat
-            .title()
-            .map_or_else(|| "Unknown chat".to_string(), html::escape));
+        let text = msg.chat.title().unwrap_or("Unknown chat");
+        return Ok(("https://t.me/".to_string(), text.to_string()));
     };
 
-    let mut result = String::new();
-    if let Some(thread_id) = msg.thread_id_ext() {
-        format_to!(
-            result,
-            "<a href=\"https://t.me/c/{chat_id}/{}/{}\">",
-            thread_id.0,
-            msg.id
-        );
+    #[allow(clippy::option_if_let_else)]
+    let url = if let Some(thread_id) = msg.thread_id_ext() {
+        format!("https://t.me/c/{chat_id}/{}/{}", thread_id.0, msg.id)
     } else {
-        format_to!(result, "<a href=\"https://t.me/c/{chat_id}/{}\">", msg.id);
-    }
+        format!("https://t.me/c/{chat_id}/{}", msg.id)
+    };
 
+    let mut text = String::new();
     let mut has_text = false;
 
     if let Some(thread_id) = msg.thread_id_ext() {
@@ -89,28 +97,27 @@ async fn render_message_link(
         if let Some(topic) = topic {
             let emojis = TopicEmojis::fetch(bot, once(&topic)).await?;
             format_to!(
-                result,
+                text,
                 "{} {}",
                 emojis.get(&topic),
-                html::escape(topic.name.as_deref().unwrap_or("Unknown topic")),
+                topic.name.as_deref().unwrap_or("Unknown topic"),
             );
         }
     }
 
     if let Some(title) = msg.chat.title() {
         if has_text {
-            format_to!(result, " @ ");
+            format_to!(text, " @ ");
         }
         has_text = true;
-        format_to!(result, "{}", html::escape(title));
+        format_to!(text, "{}", title);
     }
 
     if !has_text {
-        format_to!(result, "Unknown chat");
+        format_to!(text, "Unknown chat");
     }
 
-    format_to!(result, "</a>");
-    Ok(result)
+    Ok((url, text))
 }
 
 fn render_poll(msg: &Message, poll: &Poll, topic_link: &str) -> String {
